0fb978e97814cf321e8d8889489eb169bf813bc0
Caitlin Cooling <ccooling97@gmail.com>
2018-10-11 14:03:22 +0100

    Isolate gilded rose test, and refactor

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,17 +2,25 @@ require 'gilded_rose'
 require 'item'
 
 describe GildedRose do
+  let(:item) { double(:generic_item, name: "Item", change_quality: 0, quality: 0,
+                change_sell_in: 0, sell_in: 0) }
+  let(:item2) { double(:generic_item, change_quality: 9, quality: 9) }
+  let(:brie) { double(:aged_brie, name: "Brie", change_quality: 3, quality: 3) }
+  let(:sulfuras) { double(:sulfuras, change_quality: 80, quality: 80) }
+  let(:gr_item) { GildedRose.new([item]) }
+  let(:gr_brie) { GildedRose.new([brie]) }
+  let(:gr_item_brie) { GildedRose.new([item, brie]) }
+  let(:gr_sulfuras) { GildedRose.new([sulfuras]) }
+  let(:gr_sulfuras_brie) { GildedRose.new([sulfuras, brie]) }
+
 
   describe '#name' do
     it 'checks that the name of the object has not changed' do
-      item = GenericItem.new("Item", 1, 1)
-      gr = GildedRose.new([item])
+      gr_item
       expect(item.name).to eq "Item"
     end
     it 'checks that the names of the objects have not changed' do
-      item = GenericItem.new("Item", 1, 1)
-      brie = AgedBrie.new("Brie", 1, 2)
-      gr = GildedRose.new([item, brie])
+      gr_item_brie
       expect(item.name).to eq "Item"
       expect(brie.name).to eq "Brie"
     end
@@ -20,44 +28,24 @@ describe GildedRose do
 
   describe '#update_quality' do
     it 'updates the quality of a GenericItem' do
-      item = GenericItem.new("Item", 1, 1)
-      gr = GildedRose.new([item])
-      gr.update_quality
+      gr_item.update_quality
       expect(item.quality).to eq 0
-    end
-    it 'updates the quality of 2 generic_items' do
-      item = GenericItem.new("Item", 1, 1)
-      item2 = GenericItem.new("Item", 1, 10)
-      gr = GildedRose.new([item, item2])
-      gr.update_quality
-      expect(item.quality).to eq 0
-      expect(item2.quality).to eq 9
     end
     it 'updates the quality for brie' do
-      brie = AgedBrie.new("brie", 1, 2)
-      gr = GildedRose.new([brie])
-      gr.update_quality
+      gr_brie.update_quality
       expect(brie.quality).to eq 3
     end
     it 'updates the quality of a generic item and a brie' do
-      item = GenericItem.new("Item", 1, 1)
-      brie = AgedBrie.new("brie", 1, 2)
-      gr = GildedRose.new([item, brie])
-      gr.update_quality
+      gr_item_brie.update_quality
       expect(item.quality).to eq 0
       expect(brie.quality).to eq 3
     end
     it "it doesn't change the quality for sulfuras" do
-      sulfuras = Sulfuras.new("sulfuras", 1, 80)
-      gr = GildedRose.new([sulfuras])
-      gr.update_quality
+      gr_sulfuras.update_quality
       expect(sulfuras.quality).to eq 80
     end
     it "it doesn't change the quality for sulfuras, but does for brie" do
-      sulfuras = Sulfuras.new("sulfuras", 1, 80)
-      brie = AgedBrie.new("brie", 1, 2)
-      gr = GildedRose.new([sulfuras, brie])
-      gr.update_quality
+      gr_sulfuras_brie.update_quality
       expect(sulfuras.quality).to eq 80
       expect(brie.quality).to eq 3
     end
@@ -65,9 +53,7 @@ describe GildedRose do
 
   describe '#update_sell_in' do
     it 'updates the sell_in date for generic_item' do
-      item = GenericItem.new("Item", 1, 1)
-      gr = GildedRose.new([item])
-      gr.update_sell_in
+      gr_item.update_sell_in
       expect(item.sell_in).to eq 0
     end
   end
